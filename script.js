1d058f4161abfc17707f8567eaa6de034201543d
JingTung <ok037352085@gmail.com>
2026-03-10 14:17:56 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+const year = new Date().getFullYear()
+
+document.querySelectorAll("[data-year]").forEach(el=>{
+el.textContent = year
+})
+
+const btn = document.getElementById("menu-btn")
+const menu = document.getElementById("menu")
+
+if(btn && menu){
+
+btn.addEventListener("click",()=>{
+menu.classList.toggle("hidden")
+})
+
+}
+
+})
